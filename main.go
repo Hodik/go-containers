302d98732d91552313d519d2ca7b36bcd71e21f1
Hodik <d.khodishchenko@gmail.com>
2025-01-14 09:34:57 -0800

    update

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main() {
 	case "child":
 		child()
 	case "pull":
-		pull()
+		pull(os.Args[2])
 	default:
 		panic("bad command")
 	}
@@ -56,17 +56,27 @@ func main() {
 
 func child() {
 	log.Printf("Running: %v as %d\n", os.Args[2:], os.Getpid())
-	syscall.Sethostname([]byte("ubuntu"))
-	syscall.Chroot("/home/denis/ubuntu-fs")
+	image := os.Args[2]
+
+	image_name, filename :=	pull(image)
+	log.Println("Pulling complete: ", image_name, filename)
+	syscall.Sethostname([]byte(image_name))
+	syscall.Chroot(filename)
 	syscall.Chdir("/")
 	syscall.Mount("proc", "proc", "proc", 0, "")
 	config := syscall.ProcAttr{
 		Files: []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd()},
 	}
 
-	childPid, _, err := syscall.StartProcess(os.Args[2], os.Args[3:], &config)
+	var process_args []string 
+	if len(os.Args) >= 4 {
+		process_args = os.Args[4:]
+	} else {
+		process_args = []string{}
+	}
+	childPid, _, err := syscall.StartProcess(os.Args[3], process_args, &config)
 	if err != nil {
-		log.Fatalln("Error executing program", os.Args, err.Error())
+		log.Fatalln("Error executing program", os.Args[3], process_args, err.Error())
 	}
 	syscall.Wait4(int(childPid), nil, 0, nil)
 	syscall.Unmount("proc", 0)
@@ -74,6 +84,7 @@ func child() {
 
 func run() {
 	log.Printf("Running: %v as %d\n", os.Args[2:], os.Getpid())
+
 	config := syscall.ProcAttr{
 		Sys: &syscall.SysProcAttr{
 			Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
@@ -83,18 +94,30 @@ func run() {
 	}
 	childPid, _, err := syscall.StartProcess("/proc/self/exe", append([]string{os.Args[0], "child"}, os.Args[2:]...), &config)
 	if err != nil {
-		log.Fatalln("Failed to start a child process")
+		log.Fatalln("Failed to start a child process", err)
 	}
 	syscall.Wait4(int(childPid), nil, 0, nil)
 }
 
-func pull() {
-	image := strings.Split(os.Args[2], ":")
+func pull(_image string) (image_name string, filename string) {
+	image := strings.Split(_image, ":")
 
 	if len(image) != 2 {
 		log.Fatalln("provide image:tag")
 	}
-	pullImage(image[0], image[1], "/tmp/nginx-fs")
+	image_splitted := strings.Split(image[0], "/")
+	if len(image_splitted) > 1 {
+		image_name = image_splitted[1]
+	} else {
+		image_name = image_splitted[0]
+	}
+	filename = fmt.Sprintf("/tmp/%s-fs", image_name)
+	if _, err := os.Open(filename); err == nil {
+		log.Println(image_name, "already loaded")
+		return image_name, filename
+	}
+	pullImage(image[0], image[1], filename)
+	return image_name, filename
 }
 
 func pullImage(imageName string, tag string, outputDir string) {
